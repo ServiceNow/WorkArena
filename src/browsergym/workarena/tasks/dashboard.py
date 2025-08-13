edfde8f37350c9575c23abd30f207797cbb4e55b
Alexandre Drouin <alexandre.drouin1@gmail.com>
2025-08-13 09:50:48 -0400

    Dashboard/report tasks pull the filter from the sys property

--- src/browsergym/workarena/tasks/dashboard.py
+++ src/browsergym/workarena/tasks/dashboard.py
@@ -19,8 +19,6 @@ from ..config import (
     DASHBOARD_RETRIEVAL_VALUE_CONFIG_PATH,
     REPORT_RETRIEVAL_MINMAX_CONFIG_PATH,
     REPORT_RETRIEVAL_VALUE_CONFIG_PATH,
-    REPORT_DATE_FILTER,
-    REPORT_TIME_FILTER,
     REPORT_PATCH_FLAG,
 )
 from ..instance import SNowInstance
@@ -30,6 +28,15 @@ from .utils.utils import check_url_suffix_match
 # XXX: Some notes on plot types
 #      - We currently don't support maps because they are clickable and would require a more evolved cheat function
 SUPPORTED_PLOT_TYPES = ["area", "bar", "column", "line", "pie", "spline"]
+
+# Get report filter config
+config = SNowInstance().report_filter_config
+if config is None:
+    REPORT_DATE_FILTER = REPORT_TIME_FILTER = None
+else:
+    REPORT_DATE_FILTER = config["report_date_filter"]
+    REPORT_TIME_FILTER = config["report_time_filter"]
+del config
 
 
 class DashboardRetrievalTask(AbstractServiceNowTask, ABC):
@@ -298,6 +305,12 @@ class DashboardRetrievalTask(AbstractServiceNowTask, ABC):
 
     def setup_goal(self, page: playwright.sync_api.Page) -> Tuple[str | dict]:
         super().setup_goal(page=page)
+
+        # Check that the report filters are properly setup
+        if REPORT_DATE_FILTER is None or REPORT_TIME_FILTER is None:
+            raise RuntimeError(
+                "The report date and time filters are not set. Please run the install script to set them."
+            )
 
         # Configure task
         # ... sample a configuration
@@ -619,6 +632,12 @@ class DashboardRetrievalTask(AbstractServiceNowTask, ABC):
             The types of questions to sample from (uniformely)
 
         """
+        # Check that the report filters are properly setup
+        if REPORT_DATE_FILTER is None or REPORT_TIME_FILTER is None:
+            raise RuntimeError(
+                "The report date and time filters are not set. Please run the install script to set them."
+            )
+
         # Generate a bunch of reports based on valid table fields
         ON_THE_FLY_REPORTS = []
         for table in [
