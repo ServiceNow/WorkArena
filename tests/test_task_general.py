1f9440df254a0f9df60a6064a8563ebf5369e05a
Alexandre Drouin <alexandre.drouin@servicenow.com>
2025-11-28 11:00:33 -0500

    Reduce random_seed parameter range in tests

--- tests/test_task_general.py
+++ tests/test_task_general.py
@@ -39,7 +39,7 @@ def snow_instance_entry(request):
     before_sleep=lambda _: logging.info("Retrying due to an exception..."),
 )
 @pytest.mark.parametrize("task_entrypoint", ATOMIC_TASKS)
-@pytest.mark.parametrize("random_seed", range(3))
+@pytest.mark.parametrize("random_seed", range(1))
 @pytest.mark.slow
 def test_cheat(task_entrypoint, random_seed: int, page: Page, snow_instance_entry):
     instance = SNowInstance(
