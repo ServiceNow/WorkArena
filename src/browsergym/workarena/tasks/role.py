b7b2b82de14a98b251dc165e522fff3b254874b9
Patrice Bechard <patrice.bechard@servicenow.com>
2025-12-09 03:01:21 +0000

    rename data files, bug fixes

--- src/browsergym/workarena/tasks/role.py
+++ src/browsergym/workarena/tasks/role.py
@@ -1,9 +1,15 @@
+import json
 from typing import Any, Dict, List, Tuple
 
 import playwright.sync_api
 import requests
-from .base import AbstractServiceNowTask
 
+from ..config import (
+    ASSIGN_ROLE_TO_USER_ADMIN_CONFIG_PATH,
+    ASSIGN_ROLES_TO_USER_EXPLICIT_CONFIG_PATH,
+    ASSIGN_ROLES_TO_USER_IMPLICIT_CONFIG_PATH,
+)
+from .base import AbstractServiceNowTask
 
 
 class ServiceNowRoleTask(AbstractServiceNowTask):
@@ -11,14 +17,13 @@ class ServiceNowRoleTask(AbstractServiceNowTask):
     Generic task for role manipulation (create/edit) in a table using a Glide form.
     """
 
-    def __init__(self, seed: int, config: Dict[str, Any]) -> None:
-        super().__init__(seed)
+    def __init__(self, seed: int, fixed_config: Dict[str, Any] = None, start_rel_url: str = "/now/nav/ui/home") -> None:
+        super().__init__(seed, start_rel_url=start_rel_url)
         self.task_is_setup = False
-        self.config = config
+        self.config = fixed_config if fixed_config else self.random.choice(self.all_configs())
         self.timeout = 60000
 
     def setup_goal(self, page: playwright.sync_api.Page) -> Tuple[str, dict]:
-
         """Setup the task configuration and produce the goal."""
 
         goal = self.config["goal"]
@@ -31,11 +36,14 @@ class ServiceNowRoleTask(AbstractServiceNowTask):
 
     def validate(self, page: playwright.sync_api.Page, chat_messages: List[str]) -> Tuple[float, bool, str, dict]:
 
-
         # get relevant info from config
         user_full_name = self.config["user_full_name"]
         user_roles = self.config.get("roles", "admin")
         user_roles = [role.strip() for role in user_roles.split(",")]
+
+        # get instance url and credentials
+        instance_url = self.instance.snow_url
+        snow_username, snow_password = self.instance.snow_credentials
 
         # query instance to get user sys id
         response = requests.get(
@@ -51,7 +59,12 @@ class ServiceNowRoleTask(AbstractServiceNowTask):
         response.raise_for_status()
         record = response.json().get("result", [])
         if not record:
-            return 0
+            return (
+                0,
+                False,
+                "",
+                {"message": "The user was not found."},
+            )
         user_sys_id = record[0]["sys_id"]
 
         # query sys_user_has_role to find user roles
@@ -71,13 +84,41 @@ class ServiceNowRoleTask(AbstractServiceNowTask):
         roles = [elem["role"]["display_value"] for elem in result]
         for role in user_roles:
             if not role in roles:
-                return 0
-        return 1
+                return (
+                    0,
+                    False,
+                    "",
+                    {"message": "The role does not match."},
+                )
+        return (
+            1,
+            True,
+            "Nice work, thank you!",
+            {"message": "The record was successfully edited."},
+        )
 
     def teardown(self) -> None:
-        
+
         # go over all roles in sys_user_has_role and remove them for the given users
         pass
 
+    def all_configs(self):
+        raise NotImplementedError
 
-__TASKS__ = [ServiceNowRoleTask]
+
+class AssignRoleToUserAdminTask(ServiceNowRoleTask):
+    def all_configs(self):
+        return json.load(open(ASSIGN_ROLE_TO_USER_ADMIN_CONFIG_PATH))
+
+
+class AssignRolesToUserImplicitTask(ServiceNowRoleTask):
+    def all_configs(self):
+        return json.load(open(ASSIGN_ROLES_TO_USER_IMPLICIT_CONFIG_PATH))
+
+
+class AssignRolesToUserExplicitTask(ServiceNowRoleTask):
+    def all_configs(self):
+        return json.load(open(ASSIGN_ROLES_TO_USER_EXPLICIT_CONFIG_PATH))
+
+
+__TASKS__ = [AssignRoleToUserAdminTask, AssignRolesToUserImplicitTask, AssignRolesToUserExplicitTask]
