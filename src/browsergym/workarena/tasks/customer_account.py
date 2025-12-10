e1cc1e58d5892c6e9e19b9c171c45deb475f58f0
Patrice Bechard <patrice.bechard@servicenow.com>
2025-12-10 17:02:46 +0000

    add change request and customer account tasks

---
+++ src/browsergym/workarena/tasks/customer_account.py
@@ -0,0 +1,60 @@
+import json
+from typing import Any, Dict, List, Tuple
+
+import playwright.sync_api
+import requests
+
+from ..api.utils import HTTPError, db_delete_from_table
+from ..config import (
+    CREATE_INTERACTION_CONFIG_PATH,
+)
+from .base import AbstractServiceNowTask
+
+
+class ServiceNowCustomerAccountTask(AbstractServiceNowTask):
+
+
+    def __init__(self, seed: int, fixed_config: Dict[str, Any] = None, start_rel_url: str = "/now/nav/ui/home") -> None:
+        super().__init__(seed, start_rel_url=start_rel_url)
+        self.task_is_setup = False
+        self.config = fixed_config if fixed_config else self.random.choice(self.all_configs())
+        self.timeout = 60000
+        self.created_sysids = []
+
+    def setup_goal(self, page: playwright.sync_api.Page) -> Tuple[str, dict]:
+        """Setup the task configuration and produce the goal."""
+
+        goal = self.config["goal"]
+        info = self.config
+
+        return goal, info
+
+    def cheat(self, page: playwright.sync_api.Page, chat_messages: List[str]) -> None:
+        pass
+
+    def all_configs(self):
+        raise NotImplementedError
+
+
+class FindCustomerAccountManagerTask(ServiceNowCustomerAccountTask):
+
+    def validate(self, page: playwright.sync_api.Page, chat_messages: List[str]) -> Tuple[float, bool, str, dict]:
+
+        contact = self.config["contact"]
+
+        if contact.lower() in chat_messages[-1]["message"].lower():
+            return (
+                1,
+                True,
+                "",
+                {"message": "The customer account manager was successfully found."},
+            )
+        return (
+            0,
+            False,
+            "",
+            {"message": "The customer account manager was not found."},
+        )
+
+    def teardown(self) -> None:
+        pass
